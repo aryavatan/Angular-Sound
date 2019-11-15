67663706cc6ea953da903c260c85859ca1469d4d
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2019-11-14 19:05:57 -0500

    Followed angular tutorial to set up
    I followed the angular tutorial provided with lab 4 to initially set up my project, the link is provided here:
    
    https://www.youtube.com/watch?v=_TLhUCjY9iA&feature=youtu.be

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ReviewsComponent} from './reviews/reviews.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'reviews', component: ReviewsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
