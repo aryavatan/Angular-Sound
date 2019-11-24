f1ab4e85f52c6a03a44d0049f27d32cf84b549be
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2019-11-24 15:56:21 -0500

    Created HTTP Service

---
+++ src/app/http.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getMethod(){
+		return this.http.get('http://localhost:8080/api');
+  }
+
+  postMethod(postData){
+		return this.http.post('http://localhost:8080/api/create', postData);
+	}
+  
+}
