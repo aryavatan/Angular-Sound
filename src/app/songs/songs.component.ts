63ae65cb6b51c6001b1d2f83d81f0ed36b59a455
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2019-11-25 13:40:02 -0500

    Search Bar in Songs page working
    (2C)

--- src/app/songs/songs.component.ts
+++ src/app/songs/songs.component.ts
@@ -9,7 +9,11 @@ import { HttpService } from '../http.service';
 export class SongsComponent implements OnInit {
 
 	// For storing all records in the database
+	allSongs: Object;
 	songs: Object;
+
+	// Databinded Variables
+	searchKey: String;
 
 	constructor(private http: HttpService) { }
 
@@ -20,8 +24,36 @@ export class SongsComponent implements OnInit {
 	getSongs() {
 		this.http.getAllSongs().subscribe(data => {
 			this.songs = data;
-			console.log("Get All Songs Request Received");
-			console.log(this.songs);
+			this.allSongs = this.songs;
+			console.log('Loaded All Songs');
 		});
+	}
+
+	SearchKeyChanged() {
+		// First reset the songs object to all songs
+		this.songs = this.allSongs;
+
+		if (this.searchKey == '') {
+			return;
+		}
+
+		// Get search key and new array for the searched songs
+		let key = this.searchKey.toLowerCase();
+		let searchValues = [];
+
+		// For each song that has a keyword match, add to the searched songs array
+		Object.values(this.allSongs).forEach(song => {
+			let title = song.title.toLowerCase();
+			let artist = song.artist.toLowerCase();
+			let album = song.album.toLowerCase();
+			let year = song.year.toLowerCase();
+
+			if(title.includes(key) || artist.includes(key) || album.includes(key) || year.includes(key)){
+				searchValues.push(song);
+			}
+		});
+
+		// Set the displayed songs to the searched songs
+		this.songs = searchValues;
 	}
 }
