ad7b471511765fae46f3abeaad96fff50a053312
Arya Vatan-Abadi <arya.vatanabadi@gmail.com>
2019-11-25 17:07:21 -0500

    Created Check-Authentication Middleware in Backend

---
+++ src/server/authentication/Check-Authentication.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(" ")[1];
+		jwt.verify(token, "secret_string_that_should_be_a_lot_longer");
+		next();
+	}
+	catch (error) {
+		res.status(401).json({ message: "User Authentication Failed"});
+	}
+};
